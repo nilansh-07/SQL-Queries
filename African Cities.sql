39abdb93a42837b573c63cebd46ecf62db22b777
Nilansh Kumar <111874409+nilansh-07@users.noreply.github.com>
2023-06-19 17:58:06 +0530

    African Cities

---
+++ African Cities.sql
@@ -0,0 +1,6 @@
+/*
+Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+select city.name from city, country where city.countrycode = country.code and country.continent='Africa';
